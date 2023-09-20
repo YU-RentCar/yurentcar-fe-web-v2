0105f7b856a3bfd9a8e6e046f48f0854d28b50be
seolbotdae <seolbotdae@gmail.com>
2023-09-21 04:51:07 +0900

    Feat/#3: HomePage 상단부에 배경 이미지 추가
    HomePage 상단부에 배경 이미지 추가
     - 출처는 왼쪽 아래 표기함
    
    배경 이미지 위에 필터를 입히고 인사말 추가

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,10 +2,34 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="pt-[76px] flex flex-col items-center">
-      <div className="h-[540px] w-screen bg-blue-300"></div>
+    <div className="pt-[65px] flex flex-col items-center h-[3200px]">
+      <div className="h-[500px] w-full bg-[url(assets/Camping.jpeg)] bg-fixed bg-cover bg-center">
+        <div className="h-[500px] bg-gray-600 bg-opacity-40 relative">
+          {/* 출처 표기 */}
+          <span className="absolute bottom-0 left-0 font-medium select-none">
+            사진:
+            <a href="https://unsplash.com/ko/%EC%82%AC%EC%A7%84/4NyxhzO1iBk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Unsplash
+            </a>
+            의
+            <a href="https://unsplash.com/ko/@roadpass?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Roadpass
+            </a>
+          </span>
 
-      <div className="w-[820px] h-[90px] rounded-2xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] mt-[-54px] bg-white flex items-center justify-between relative">
+          {/* 사진 내부 문구 */}
+          <span className="absolute left-[13%] top-[120px] font-black text-[80px] text-white select-none">
+            즐거운 여행!
+          </span>
+          <span className="absolute left-[13%] top-[240px] font-black text-[75px] text-white select-none">
+            <span className="text-sky-600">Y</span>
+            <span className="text-amber-900">U</span> 렌트카와 함께 준비하세요!
+          </span>
+        </div>
+      </div>
+
+      {/* 선택 메뉴 */}
+      <div className="w-[820px] h-[90px] rounded-2xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] mt-[-54px] bg-white flex items-center justify-between z-10">
         <div className="ml-[10px] w-[210px] h-[70px] bg-sky-50 rounded-xl border-[1px] border-black hover:border-[3px] hover:border-blue-400 select-none cursor-pointer">
           <div className="flex items-center justify-center w-full h-full">
             <p className="text-2xl font-black">대구 수성구점</p>
