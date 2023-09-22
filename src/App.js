e4eb8fc5d48b68f83f3a4ab3c128b9c7c51e44e7
seolbotdae <seolbotdae@gmail.com>
2023-09-22 19:45:01 +0900

    Feat/#3: App.js 에 Home 페이지 라우터 추가
    App.js 에 Home 페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import NavContainer from "components/Nav/NavContainer";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import HomeContainer from "pages/Home/HomeContainer";
 
 function App() {
-  return <Routes></Routes>;
+  return (
+    <>
+      <NavContainer></NavContainer>
+      <Routes>
+        <Route path="/" element={<HomeContainer />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
