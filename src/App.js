520afb2dca1300ee558f661b8f3c0ec14f639ed4
yh-project <yoha6865@gmail.com>
2023-09-24 15:17:11 +0900

    Feat/#11: 마이페이지 라우터 설정 "/mypage" 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import NavContainer from "components/Nav/NavContainer";
+import MyPageContainer from "pages/MyPage/MyPageContainer";
 
 function App() {
-  return <Routes></Routes>;
+  return (
+    <>
+      <NavContainer></NavContainer>
+      <Routes>
+        <Route path="/mypage" element={<MyPageContainer />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
