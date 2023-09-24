7ce3b83ef808d11450fa2171a414296ed7d7b5a8
yh-project <yoha6865@gmail.com>
2023-09-24 20:44:49 +0900

    Feat/#22: 주석 추가
    사용자 면허 정보 주석 추가

--- src/pages/MyPage/components/LicenseContainer.js
+++ src/pages/MyPage/components/LicenseContainer.js
@@ -2,6 +2,9 @@ import { useRecoilValue } from "recoil";
 import { userLicenseAtom } from "recoil/userLicenseAtom";
 import License from "./License";
 
+/**
+ * licenseInfo : 사용자 면허 정보
+ */
 const LicenseContainer = () => {
   const licenseInfo = useRecoilValue(userLicenseAtom);
   return (
