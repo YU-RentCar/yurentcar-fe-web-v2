87c47db76934f931ca83fc44c62c1026b966e775
yh-project <yoha6865@gmail.com>
2023-09-24 16:42:40 +0900

    Feat/#11: 마이페이지 알림창(Alert) 구현
    마이페이지에서 알림창(Alert)를 보여주기 위한 로직 구현

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,13 +1,17 @@
 import ReservationContainer from "./components/ReservationContainer";
 import UserInfoContainer from "./components/UserInfoContainer";
+import AlertContainer from "./AlertContainer";
 
-const MyPage = () => {
+const MyPage = ({ alertState }) => {
   return (
     <>
       <div className="w-[1140px] mx-auto mt-[176px]">
-        <ReservationContainer /> {/* 예약 대기 정보 */}
-        <UserInfoContainer /> {/* 사용자 정보 */}
+        {/* 예약 대기 정보 */}
+        <ReservationContainer />
+        {/* 사용자 정보 */}
+        <UserInfoContainer />
       </div>
+      {alertState ? <AlertContainer /> : null}
     </>
   );
 };
--- src/pages/MyPage/MyPageContainer.js
+++ src/pages/MyPage/MyPageContainer.js
@@ -1,9 +1,15 @@
+import { useRecoilValue } from "recoil";
 import MyPage from "./MyPage";
+import { alertAtom } from "recoil/alertAtom";
 
+/**
+ * alertState : 알림창 상태
+ */
 const MyPageContainer = () => {
+  const alertState = useRecoilValue(alertAtom).state;
   return (
     <div>
-      <MyPage></MyPage>
+      <MyPage alertState={alertState}></MyPage>
     </div>
   );
 };
