55d98546039f241ade6ba957aad43dccf231484d
seolbotdae <seolbotdae@gmail.com>
2023-10-05 20:31:15 +0900

    Feat/#48: 지점별 공지사항 컴포넌트 추가
    차량 검색 레이아웃 추가
    
    지점별 공지사항 컴포넌트 생성
     - material tailwind의 Carousel 컴포넌트를 사용

--- src/pages/CarSearch/CarSearch.js
+++ src/pages/CarSearch/CarSearch.js
@@ -3,6 +3,7 @@ import React from "react";
 import { usePopUp } from "utils/usePopUp";
 import SelectStore from "popUp/SelectStore";
 import SelectDateTime from "popUp/SelectDateTime";
+import NoticeCarousel from "./NoticeCarousel";
 
 const CarSearch = () => {
   const storePopUp = usePopUp("CarSearch/SelectStore");
@@ -13,6 +14,31 @@ const CarSearch = () => {
       <div className="mt-[65px] flex flex-col items-center">
         <div className="w-[860px] h-[70px] rounded-2xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] mt-[20px] bg-white flex items-center justify-between z-10">
           <Finder storePopUp={storePopUp} dateTimePopUp={dateTimePopUp} />
+        </div>
+
+        <div className="w-[1140px] h-[1281px] bg-sky-200 mt-[20px]">
+          <div className="w-[264px] bg-blue-300">
+            {/* 사용자 선호 설정 */}
+            <div className="w-full h-[414px] bg-blue-800"></div>
+            {/* 공지사항 */}
+            <div className="w-full bg-sky-100">
+              {/* 어떤 지점 공지사항 */}
+              <div className="flex flex-col items-center justify-center">
+                <span>
+                  <span>대구 수성구</span>점<br />
+                </span>
+                <span>공지사항</span>
+              </div>
+
+              {/* 공지사항 슬라이더 */}
+              <div className="h-[485px]">
+                <NoticeCarousel />
+              </div>
+            </div>
+          </div>
+
+          {/* 차량 리스트 */}
+          <div></div>
         </div>
       </div>
 
---
+++ src/pages/CarSearch/NoticeCarousel.js
@@ -0,0 +1,121 @@
+import { Carousel, Typography, Button } from "@material-tailwind/react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+export default function CarouselWithContent() {
+  return (
+    <Carousel
+      className="rounded-xl"
+      prevArrow={prevArrowCustom}
+      nextArrow={nextArrowCustom}
+      navigation={navigationCustom}
+      autoplay={true}
+      loop={true}
+    >
+      <div className="relative w-full h-full fill-black">
+        <div className="absolute inset-0 grid w-full h-full bg-blue-100 place-items-center fill-black">
+          <div className="flex flex-col items-center justify-start w-full h-full">
+            <img
+              className="w-[160px] h-[114px] object-cover mt-[23px] rounded-xl"
+              src="https://thecatapi.com/api/images/get?format=src&type=gif"
+              alt=""
+            />
+
+            <h1 className="p-4 font-bold text-center text-blue-900">
+              Take a look Take a look
+            </h1>
+
+            <p className=" w-[200px] line-clamp-6">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+              voluptas iure quas maiores? Itaque molestias voluptatum architecto
+              sapiente! Harum enim reprehenderit quo quia temporibus dignissimos
+              consequuntur nostrum velit eos nulla!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="relative w-full h-full">
+        <div className="absolute inset-0 grid w-full h-full bg-blue-100 place-items-center fill-black">
+          <div className="flex flex-col items-center justify-start w-full h-full">
+            <img
+              className="w-[160px] h-[114px] object-cover mt-[23px] rounded-xl"
+              src="https://thecatapi.com/api/images/get?format=src&type=gif"
+              alt=""
+            />
+
+            <h1 className="p-4 font-bold text-center text-blue-900">
+              Take a look Take a look
+            </h1>
+
+            <p className=" w-[200px] line-clamp-6">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+              voluptas iure quas maiores? Itaque molestias voluptatum architecto
+              sapiente! Harum enim reprehenderit quo quia temporibus dignissimos
+              consequuntur nostrum velit eos nulla!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="relative w-full h-full">
+        <div className="absolute inset-0 grid w-full h-full bg-blue-100 place-items-center fill-black">
+          <div className="flex flex-col items-center justify-start w-full h-full">
+            <img
+              className="w-[160px] h-[114px] object-cover mt-[23px] rounded-xl"
+              src="https://thecatapi.com/api/images/get?format=src&type=gif"
+              alt=""
+            />
+
+            <h1 className="p-4 font-bold text-center text-blue-900">
+              Take a look Take a look
+            </h1>
+
+            <p className=" w-[200px] line-clamp-6">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+              voluptas iure quas maiores? Itaque molestias voluptatum architecto
+              sapiente! Harum enim reprehenderit quo quia temporibus dignissimos
+              consequuntur nostrum velit eos nulla!
+            </p>
+          </div>
+        </div>
+      </div>
+    </Carousel>
+  );
+}
+
+function prevArrowCustom({ loop, handlePrev, firstIndex }) {
+  return (
+    <button
+      onClick={handlePrev}
+      disabled={!loop && firstIndex}
+      className="!absolute top-[80px] left-1 -translate-y-2/4 rounded-full select-none transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] text-white hover:bg-white/10 active:bg-white/30 grid place-items-center"
+    >
+      <MdChevronLeft color="black" size={30} className="-ml-1 h-7 w-7" />
+    </button>
+  );
+}
+
+function nextArrowCustom({ loop, handleNext, lastIndex }) {
+  return (
+    <button
+      onClick={handleNext}
+      disabled={!loop && lastIndex}
+      className="!absolute top-[80px] right-1 -translate-y-2/4 rounded-full select-none transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] text-white hover:bg-white/10 active:bg-white/30 grid place-items-center"
+    >
+      <MdChevronRight color="black" size={30} className="ml-1 h-7 w-7" />
+    </button>
+  );
+}
+
+function navigationCustom({ setActiveIndex, activeIndex, length }) {
+  return (
+    <div className="absolute z-50 flex gap-2 bottom-4 left-2/4 -translate-x-2/4">
+      {new Array(length).fill("").map((_, i) => (
+        <span
+          key={i}
+          className={`block h-3 w-3 cursor-pointer rounded-full transition-colors content-[''] border-2 border-black ${
+            activeIndex === i ? "bg-blue-500" : "bg-white"
+          }`}
+          onClick={() => setActiveIndex(i)}
+        />
+      ))}
+    </div>
+  );
+}
