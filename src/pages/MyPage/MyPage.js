350a2aaedbc3066fd64fef2bdf493663365afa66
yh-project <yoha6865@gmail.com>
2023-10-11 17:25:34 +0900

    Fix/#92: Alert 사용 방식 변경
    App.js 에서 하나로 관리

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -4,17 +4,13 @@ import PreferOption from "./PreferOption";
 import Record from "./Record";
 import License from "./License";
 import Account from "./Account";
-import Alert from "popUp/Alert";
 import Point from "popUp/MyPage/Point";
 import Resv from "popUp/MyPage/Resv";
 import Quit from "popUp/MyPage/Quit";
 import { usePopUp } from "utils/usePopUp";
-import { alertAtom } from "recoil/alertAtom";
-import { useRecoilValue } from "recoil";
 import { useState } from "react";
 
 const MyPage = () => {
-  const alertState = useRecoilValue(alertAtom).state; // Alert 제어
   const popUpQuit = usePopUp("MyPage/Quit"); // Quit 팝업 제어
   const popUpPoint = usePopUp("MyPage/Point"); // Point 팝업 제어
   const popUpResv = usePopUp("MyPage/Resv"); // Resv 팝업 제어
@@ -36,7 +32,6 @@ const MyPage = () => {
         {/* 사용자 계정 관리 */}
         <Account />
       </div>
-      {alertState ? <Alert /> : null}
       {popUpQuit.isClicked ? <Quit /> : null}
       {popUpPoint.isClicked ? <Point /> : null}
       {popUpResv.isClicked ? <Resv /> : null}
