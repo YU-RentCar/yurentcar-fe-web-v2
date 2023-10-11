a18846b38b93893045e9b91277067259ea5b76cb
yh-project <yoha6865@gmail.com>
2023-10-11 17:54:10 +0900

    Fix/#92: 포인트 사용 내역 팝업 스크롤
    y축 스크롤 지정

--- src/popUp/MyPage/Point.js
+++ src/popUp/MyPage/Point.js
@@ -49,7 +49,7 @@ const Point = () => {
               onClick={() => popUpPoint.toggle()}
             />
           </div>
-          <div className="w-[870px] h-[500px] rounded-2xl bg-sky-50 mt-4 pt-2">
+          <div className="w-[870px] h-[500px] rounded-2xl bg-sky-50 mt-4 pt-2 overflow-y-scroll">
             {/* 포인트 적립 / 차감 내역 */}
             {pointRecord.map((v, i) => {
               return (
