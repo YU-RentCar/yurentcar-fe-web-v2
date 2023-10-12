970abcffbcf6c1947f8224b40681e42db025a9d7
seolbotdae <seolbotdae@gmail.com>
2023-10-12 14:43:57 +0900

    Feat/#94: 차량 리스트 서버로 요청하여 렌더링
    차량 리스트 서버로 요청하여 렌더링
    
    CarCard 컴포넌트를 동적으로 변경 가능하도록 변경

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -3,45 +3,45 @@ import { useRecoilValue } from "recoil";
 import { carAtom } from "recoil/carAtom";
 import { Chip } from "@material-tailwind/react";
 
-const CarCard = () => {
-  const carInfo = useRecoilValue(carAtom); // 차량 정보
+const CarCard = ({ name, number, odo, price, imageURI, discountRatio }) => {
   return (
     <div className="w-[270px] h-[376px] rounded-2xl bg-white mt-8 hover:shadow-figma mx-auto border-2">
       {/* 차량 사진 */}
       <img
-        src={Car}
+        // 일시적으로 고양이 움짤로 대체
+        src="https://thecatapi.com/api/images/get?format=src&type=gif"
         alt="차량 사진"
         className="object-cover h-[164px] w-full rounded-t-2xl"
       ></img>
       {/* 차량 해쉬태그 */}
       <div className="flex flex-wrap items-center justify-center mt-2">
         <Chip
-          value={`# ${carInfo.car}`}
+          value={`# ${name}`}
           className="text-[13px] font-semibold w-[88px] h-[22px] flex justify-center items-center rounded-[5px] m-1 bg-blue-500"
         />
         <Chip
-          value={`# ${carInfo.number}`}
+          value={`# ${number}`}
           className="text-[13px] font-semibold w-[88px] h-[22px] flex justify-center items-center rounded-[5px] m-1 bg-blue-400"
         />
         <Chip
-          value={`# ${carInfo.odo}km`}
+          value={`# ${odo}km`}
           className="text-[13px] font-semibold w-[88px] h-[22px] flex justify-center items-center rounded-[5px] m-1 bg-blue-300 text-blue-900"
         />
         <Chip
-          value={`# ${carInfo.price / 10000}만원대`}
+          value={`# ${price / 10000}만원대`}
           className="text-[13px] font-semibold w-[88px] h-[22px] flex justify-center items-center rounded-[5px] m-1 bg-blue-200 text-blue-900"
         />
       </div>
       {/* 차량 이름 */}
-      <div className="pl-4 mt-4 text-2xl font-extrabold">{carInfo.car}</div>
+      <div className="pl-4 mt-4 text-2xl font-extrabold">{name}</div>
       {/* 차량 가격 */}
       <div className="pl-4 mt-4 text-xl font-bold">
         <span className="line-through text-slate-500">{`₩${
-          carInfo.price * 1.3
+          price * (1.0 + discountRatio / 100)
         }`}</span>
-        <span className="ml-2 text-red-500">-30%</span>
+        <span className="ml-2 text-red-500">{-`${discountRatio}%`}</span>
       </div>
-      <div className="flex justify-end pr-4 mt-2 font-extrabold text-[28px] text-blue-500">{`₩${carInfo.price}`}</div>
+      <div className="flex justify-end pr-4 mt-2 font-extrabold text-[28px] text-blue-500">{`₩${price}`}</div>
     </div>
   );
 };
--- src/pages/CarSearch/CarSearch.js
+++ src/pages/CarSearch/CarSearch.js
@@ -11,6 +11,8 @@ import PreferOption from "./PreferOption";
 import CarDetail from "popUp/CarSearch/CarDetail";
 import { useEffect, useState, useRef } from "react";
 import { finderAtom } from "recoil/finderAtom";
+import dayjs from "dayjs";
+import { getCarInfoList } from "api/homeAxios";
 
 const CarSearch = () => {
   const storePopUp = usePopUp("CarSearch/SelectStore");
@@ -22,8 +24,9 @@ const CarSearch = () => {
     useRecoilState(userPreferSelector); // 사용자의 선호 옵션 정보
   const [preferTitles, _] = useState(["차량 크기", "유종", "구동기"]); // 옵션 타이틀
 
-  const [currentStore, setCurrentStore] = useState(null);
   const finderInfo = useRecoilValue(finderAtom);
+
+  const [carInfoList, setCarInfoList] = useState(null);
 
   /* 변경 정보 수집 함수 */
   const gatherInfo = () => {
@@ -42,8 +45,35 @@ const CarSearch = () => {
     return newPrefer;
   };
 
+  // 차량 리스트를 조회
   useEffect(() => {
-    setCurrentStore(finderInfo);
+    // 차량 리스트 조회 시 필요한 정보들 생성
+    const infos = {};
+
+    infos.startDate = String(
+      dayjs(finderInfo.startDate).format("YYYY. MM. DD. ") +
+        finderInfo.startTime
+    );
+    infos.endDate = String(
+      dayjs(finderInfo.endDate).format("YYYY. MM. DD. ") + finderInfo.endTime
+    );
+    infos.carSizes = [true, true, true, true];
+    infos.minCount = 1;
+    infos.oilTypes = [true, true, true, true];
+    infos.transmissions = [true, true];
+    // infos.branchName = "IT관점";
+    infos.branchName = finderInfo.store;
+    infos.siDo = finderInfo.province;
+    // infos.siDo = "경상북도";
+
+    getCarInfoList(infos)
+      .then((response) => {
+        console.log("CarSearch/getCarCard", response.data);
+        setCarInfoList(response.data);
+      })
+      .catch((error) => {
+        console.log("CarSearch/getCarCard", error.response);
+      });
   }, []);
 
   return (
@@ -124,17 +154,25 @@ const CarSearch = () => {
 
           {/* 차량 리스트 */}
           <div className="w-[860px] h-[1000px] pb-5 grid grid-cols-3 overflow-y-scroll">
-            {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((v, i) => {
-              return (
-                <div
-                  onClick={() => {
-                    carDetailPopUp.toggle();
-                  }}
-                >
-                  <CarCard></CarCard>
-                </div>
-              );
-            })}
+            {carInfoList
+              ? carInfoList.map((v, i) => {
+                  return (
+                    <div
+                      onClick={() => {
+                        carDetailPopUp.toggle();
+                      }}
+                    >
+                      {/* 추후 빠진 props 추가할 것 */}
+                      <CarCard
+                        name={v.carName}
+                        number={v.carNumber}
+                        odo={v.totalDistance}
+                        price={v.price}
+                      ></CarCard>
+                    </div>
+                  );
+                })
+              : null}
           </div>
         </div>
       </div>
