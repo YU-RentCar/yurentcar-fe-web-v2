7b351e2dc4cb1bbd9a28d1aedce5aaccee0c0523
yh-project <yoha6865@gmail.com>
2023-10-13 18:01:38 +0900

    Feat/#96: 기본 정보 조회 useEffect 종속성 배열 추가
    무한 호출 방지

--- src/pages/Reservation/DefaultInfo.js
+++ src/pages/Reservation/DefaultInfo.js
@@ -33,7 +33,7 @@ const DefaultInfo = () => {
       .catch((error) => {
         console.log("예약 / 기본정보에러 : ", error.response);
       });
-  });
+  }, []);
   return (
     <div className="flex flex-col items-center w-full py-8 mt-12 bg-sky-50 rounded-2xl shadow-figma">
       {/* 타이틀 */}
