4e8ec60b0d4e667c4d569eaf4b34057c163588c1
yh-project <yoha6865@gmail.com>
2023-11-02 18:44:49 +0900

    Fix/#139: 최종 결제 UI 수정
    전체 높이 및 글자 크기 조정

--- src/pages/Reservation/Pay.js
+++ src/pages/Reservation/Pay.js
@@ -9,41 +9,41 @@ const Pay = () => {
   const alert = useAlert(); // Alert 제어
   const rentInfo = useRecoilValue(rentAtom); // 예약 결제 금액 정보
   return (
-    <div className="w-[300px] h-[360px] rounded-2xl fixed top-[576px] right-[390px] bg-sky-50 shadow-figma flex flex-col items-center">
+    <div className="w-[300px] h-[300px] rounded-2xl fixed top-[576px] right-[390px] bg-sky-50 shadow-figma flex flex-col items-center">
       <div className="flex items-center justify-center w-full mt-4 text-2xl font-bold text-blue-800">
         최종 결제
       </div>
-      <div className="w-[280px] h-[300px] flex flex-col justify-between items-center mt-4">
+      <div className="w-[280px] h-[260px] flex flex-col justify-between items-center mt-4">
         {/* 금액 결산 */}
-        <div className="flex flex-col items-center justify-around w-full h-full px-4 py-4 bg-white rounded-2xl">
-          <div className="w-full h-[35px] flex justify-between items-center">
-            <span className="text-xl font-bold">할인 전</span>
-            <span className="text-xl font-bold line-through">
+        <div className="flex flex-col items-center justify-around w-full h-[200px] px-4 py-2 bg-white rounded-2xl">
+          <div className="w-full h-[30px] flex justify-between items-center">
+            <span className="text-lg font-bold">할인 전</span>
+            <span className="text-lg font-bold line-through">
               {rentInfo.beforePrice}원
             </span>
           </div>
-          <div className="w-full h-[35px] flex justify-between items-center">
-            <span className="text-xl font-bold">할인 후</span>
-            <span className="text-xl font-bold text-red-500">
+          <div className="w-full h-[30px] flex justify-between items-center">
+            <span className="text-lg font-bold">할인 후</span>
+            <span className="text-lg font-bold text-red-500">
               {rentInfo.afterPrice}원
             </span>
           </div>
-          <div className="w-full h-[35px] flex justify-between items-center">
-            <span className="text-xl font-bold">보험료</span>
-            <span className="text-xl font-bold text-blue-600">
+          <div className="w-full h-[30px] flex justify-between items-center">
+            <span className="text-lg font-bold">보험료</span>
+            <span className="text-lg font-bold text-blue-600">
               +{rentInfo.insurance < 0 ? 0 : rentInfo.insurance}원
             </span>
           </div>
-          <div className="w-full h-[35px] flex justify-between items-center">
-            <span className="text-xl font-bold">포인트</span>
-            <span className="text-xl font-bold text-red-500">
+          <div className="w-full h-[30px] flex justify-between items-center">
+            <span className="text-lg font-bold">포인트</span>
+            <span className="text-lg font-bold text-red-500">
               -{rentInfo.point < 0 ? 0 : rentInfo.point}P
             </span>
           </div>
           <hr className="w-full border-2 border-black" />
-          <div className="w-full h-[35px] flex justify-between items-center">
-            <span className="text-xl font-bold">결제금액</span>
-            <span className="text-xl font-bold text-blue-600">
+          <div className="w-full h-[30px] flex justify-between items-center">
+            <span className="text-lg font-bold">결제금액</span>
+            <span className="text-lg font-bold text-blue-600">
               {rentInfo.afterPrice +
                 (rentInfo.insurance < 0 ? 0 : rentInfo.insurance) -
                 (rentInfo.point < 0 ? 0 : rentInfo.point)}
@@ -52,7 +52,7 @@ const Pay = () => {
         </div>
         {/* 최종 결제 버튼 */}
         <button
-          className="flex items-center justify-center w-full h-12 text-2xl font-bold rounded-lg bg-amber-400 hover:shadow-figma"
+          className="flex items-center justify-center w-full h-12 text-xl font-bold rounded-lg bg-amber-400 hover:shadow-figma"
           onClick={() => {
             if (rentInfo.insurance < 0) alert.onAndOff("보험을 선택해주세요");
             else if (rentInfo.point < 0)
