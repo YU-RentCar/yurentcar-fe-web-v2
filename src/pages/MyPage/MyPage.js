b1c62effd2fa06075b476732dff20c5991d271c1
yh-project <yoha6865@gmail.com>
2023-11-02 11:16:22 +0900

    Fix/#137: 마이페이지 SideMenu 추가
    대기 중인 예약이 있는지에 따른 처리
     - withResv props 전달

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,6 +1,7 @@
 import { usePopUp } from "utils/usePopUp";
 import { useState, useEffect } from "react";
 import { useAuth } from "utils/useAuth";
+import SideMenu from "./SideMenu";
 import Reservation from "./Reservation";
 import UserInfo from "./UserInfo";
 import PreferOption from "./PreferOption";
@@ -18,26 +19,30 @@ const MyPage = () => {
   const popUpPoint = usePopUp("MyPage/Point"); // Point 팝업 제어
   const popUpResv = usePopUp("MyPage/Resv"); // Resv 팝업 제어
   const popUpReview = usePopUp("MyPage/Review"); // Review 팝업 제어
-  const [resvState, setResvState] = useState(true); // 예약 대기 중인 정보의 유무
+  const [resvState, setResvState] = useState(false); // 예약 대기 중인 정보의 유무
   useEffect(() => {
     auth.loginCheck();
   });
   return (
     <>
-      <div className="w-[1140px] mx-auto mt-[176px]">
-        {/* 예약 대기 정보 */}
-        {/* 대기 중인 예약이 없을 경우 보이지 않음 */}
-        {resvState ? <Reservation setResvState={setResvState} /> : null}
-        {/* 사용자 정보 */}
-        <UserInfo />
-        {/* 사용자 선호 차량 옵션 */}
-        <PreferOption />
-        {/* 사용자 사용 내역 */}
-        <Record />
-        {/* 사용자 면허 정보 */}
-        <License />
-        {/* 사용자 계정 관리 */}
-        <Account />
+      <div className="w-[1140px] mx-auto mt-36 relative">
+        {/* 사이드 메뉴 */}
+        <SideMenu withResv={resvState} />
+        <div className="w-[800px] absolute right-0 top-0">
+          {/* 예약 대기 정보 */}
+          {/* 대기 중인 예약이 없을 경우 보이지 않음 */}
+          {resvState ? <Reservation setResvState={setResvState} /> : null}
+          {/* 사용자 정보 */}
+          <UserInfo />
+          {/* 사용자 선호 차량 옵션 */}
+          <PreferOption />
+          {/* 사용자 사용 내역 */}
+          <Record />
+          {/* 사용자 면허 정보 */}
+          <License />
+          {/* 사용자 계정 관리 */}
+          <Account />
+        </div>
       </div>
       {popUpQuit.isClicked ? <Quit /> : null}
       {popUpPoint.isClicked ? <Point /> : null}
