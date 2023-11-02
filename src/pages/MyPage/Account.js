7a31513c37c21dacbcb27b6716eeb9719e3dde74
yh-project <yoha6865@gmail.com>
2023-11-02 11:25:26 +0900

    Fix/#137: 마이페이지 계정 관리 Fix
    UI 크기 조정
    
    SideMenu 를 위한 id 추가

--- src/pages/MyPage/Account.js
+++ src/pages/MyPage/Account.js
@@ -6,45 +6,44 @@ const Account = () => {
   const popUpInfo = usePopUp("MyPage/Quit"); // 팝업 제어
   const nav = useNavigate(); // 경로 이동
   return (
-    <div className="flex flex-col items-center w-full py-8 mt-12 bg-sky-50 rounded-2xl shadow-figma">
+    <div
+      className="flex flex-col items-center w-full py-8 mt-12 mb-40 bg-sky-50 rounded-2xl shadow-figma"
+      id="MyPage/Account"
+    >
       {/* 타이틀 */}
-      <div className="w-[1010px] h-[70px] flex justify-between items-center">
-        <span className="text-blue-800 text-[45px] font-extrabold">
+      <div className="w-[700px] h-[35px] flex justify-between items-center">
+        <span className="text-blue-800 text-[30px] font-extrabold">
           계정 관리
         </span>
       </div>
       {/* 회원 탈퇴 */}
-      <div className="w-[1010px] h-40 bg-white rounded-2xl flex items-center px-8 mt-7">
-        <div className="flex items-center justify-between w-full h-24 text-2xl font-bold">
-          <div>회원 탈퇴</div>
-          <button
-            className="w-56 h-16 text-2xl font-bold text-white bg-red-500 rounded-2xl"
-            onClick={() => popUpInfo.toggle()}
-          >
-            탈퇴하기
-          </button>
-        </div>
+      <div className="w-[700px] h-24 bg-white rounded-2xl flex justify-between items-center px-8 py-[15px] mt-4">
+        <div className="text-xl font-bold">회원 탈퇴</div>
+        <button
+          className="w-32 text-xl font-bold text-white bg-red-500 h-11 rounded-xl hover:shadow-figma"
+          onClick={() => popUpInfo.toggle()}
+        >
+          탈퇴하기
+        </button>
       </div>
       {/* 로그아웃 */}
-      <div className="w-[1010px] h-40 bg-white rounded-2xl flex items-center px-8 mt-7">
-        <div className="flex items-center justify-between w-full h-24 text-2xl font-bold">
-          <div>로그아웃</div>
-          <button
-            className="w-56 h-16 text-2xl font-bold text-red-500 bg-sky-200 rounded-2xl"
-            onClick={async () => {
-              await logout()
-                .then((response) => {
-                  console.log("마이페이지 / 로그아웃 : ", response.data);
-                  nav("/auth");
-                })
-                .catch((error) =>
-                  console.log("마이페이지 / 로그아웃에러 : ", error.response)
-                );
-            }}
-          >
-            로그아웃
-          </button>
-        </div>
+      <div className="w-[700px] h-24 bg-white rounded-2xl flex justify-between items-center px-8 py-[15px] mt-4">
+        <div className="text-xl font-bold">로그아웃</div>
+        <button
+          className="w-32 text-xl font-bold text-red-500 h-11 bg-sky-200 rounded-xl hover:shadow-figma"
+          onClick={async () => {
+            await logout()
+              .then((response) => {
+                console.log("마이페이지 / 로그아웃 : ", response.data);
+                nav("/auth");
+              })
+              .catch((error) =>
+                console.log("마이페이지 / 로그아웃에러 : ", error.response)
+              );
+          }}
+        >
+          로그아웃
+        </button>
       </div>
     </div>
   );
