c2b3cce2402eedf4706e036dc7053854b16a3e68
yh-project <yoha6865@gmail.com>
2023-11-02 11:22:51 +0900

    Fix/#137: 마이페이지 선호 차량 Fix
    UI 크기 조정
    
    SideMenu 를 위한 id 추가
    
    UI 구현 방식 변경
     - Material Tailwind CSS Input -> HTML input 사용
     - 스타일 지정의 편의를 위함

--- src/pages/MyPage/PreferContent.js
+++ src/pages/MyPage/PreferContent.js
@@ -1,4 +1,4 @@
-import { Checkbox, Input } from "@material-tailwind/react";
+import { Checkbox } from "@material-tailwind/react";
 
 const PreferContent = ({
   title,
@@ -9,46 +9,43 @@ const PreferContent = ({
   gatherInfo,
 }) => {
   return (
-    <div className="w-[1010px] h-40 bg-white rounded-2xl flex items-center px-8 mt-7">
-      <div className="flex flex-col justify-between w-full h-24 text-2xl font-bold">
-        <div className="text-slate-400">{title}</div>
-        {title !== "최소 인원" ? (
-          /* 차량 크기, 유종, 구동기 -> Checkbox */
-          <div className="flex items-center mx-auto">
-            {content.map((v, i) => {
-              return (
-                <div className="w-32 mx-4" key={i}>
-                  <Checkbox
-                    id={v}
-                    label={v}
-                    ripple={true}
-                    labelProps={{ className: "font-semibold text-black" }}
-                    checked={userPrefer[i]}
-                    onChange={() => {
-                      setUserPrefer(gatherInfo());
-                    }}
-                  />
-                </div>
-              );
-            })}
-          </div>
-        ) : (
-          /* 최소 인원 -> Input */
-          <div className="flex items-center w-48 h-full mx-auto">
-            <Input
-              id="minCount"
-              type="number"
-              className="!text-3xl !font-bold !text-black !h-[65px]"
-              value={minCount}
-              label="숫자만 입력가능"
-              labelProps={{ className: "border-black text-lg" }}
-              onChange={() => {
-                setUserPrefer(gatherInfo());
-              }}
-            />
-          </div>
-        )}
-      </div>
+    <div className="w-[700px] h-24 bg-white rounded-2xl flex flex-col justify-between px-8 py-[15px] mt-4">
+      <div className="font-bold text-slate-400">{title}</div>
+      {title !== "최소 인원" ? (
+        /* 차량 크기, 유종, 구동기 -> Checkbox */
+        <div className="flex items-center mx-auto">
+          {content.map((v, i) => {
+            return (
+              <div className="mx-4 w-28" key={i}>
+                <Checkbox
+                  id={v}
+                  label={v}
+                  ripple={true}
+                  labelProps={{ className: "font-semibold text-black" }}
+                  checked={userPrefer[i]}
+                  onChange={() => {
+                    setUserPrefer(gatherInfo());
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        /* 최소 인원 -> Input */
+        <div className="flex items-center w-32 mx-auto">
+          <input
+            id="minCount"
+            type="number"
+            className="w-full px-2 text-xl font-bold text-black border border-black rounded-lg"
+            value={minCount}
+            label="숫자만 입력가능"
+            onChange={() => {
+              setUserPrefer(gatherInfo());
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 };
