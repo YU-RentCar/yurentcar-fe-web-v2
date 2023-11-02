f0877581b5d10c9419db21048b372791d92bec7e
yh-project <yoha6865@gmail.com>
2023-11-02 11:21:18 +0900

    Fix/#137: 마이페이지 기본 정보 Fix
    UI 크기 조정
    
    SideMenu 를 위한 id 추가

--- src/pages/MyPage/UserInfo.js
+++ src/pages/MyPage/UserInfo.js
@@ -17,37 +17,38 @@ const UserInfo = () => {
       );
   }, []);
   return (
-    <>
-      <div className="flex flex-col items-center w-full py-8 mt-12 bg-sky-50 rounded-2xl shadow-figma">
-        {/* 타이틀 */}
-        <div className="w-[1010px] h-[70px] flex justify-between items-center">
-          <span className="text-blue-800 text-[45px] font-extrabold">
-            기본 정보
-          </span>
-        </div>
-        {/* 사용자 정보 - 이름 */}
-        <UserContent title="이름" content={userInfo.name} />
-        {/* 사용자 정보 - 닉네임 */}
-        {isChanging ? (
-          <UserContent
-            title="닉네임"
-            content={userInfo.nickname}
-            changeSetter={setIsChanging}
-          />
-        ) : (
-          <UserNickChange
-            before={userInfo.nickname}
-            changeSetter={setIsChanging}
-            userInfo={userInfo}
-            setUserInfo={setUserInfo}
-          />
-        )}
-        {/* 사용자 정보 - 전화번호 */}
-        <UserContent title="전화번호" content={userInfo.phoneNumber} />
-        {/* 사용자 정보 - 이메일 */}
-        <UserContent title="이메일" content={userInfo.username} />
+    <div
+      className="flex flex-col items-center w-full py-8 bg-sky-50 rounded-2xl shadow-figma"
+      id="MyPage/UserInfo"
+    >
+      {/* 타이틀 */}
+      <div className="w-[700px] h-[35px] flex justify-between items-center">
+        <span className="text-blue-800 text-[30px] font-extrabold">
+          기본 정보
+        </span>
       </div>
-    </>
+      {/* 사용자 정보 - 이름 */}
+      <UserContent title="이름" content={userInfo.name} />
+      {/* 사용자 정보 - 닉네임 */}
+      {isChanging ? (
+        <UserContent
+          title="닉네임"
+          content={userInfo.nickname}
+          changeSetter={setIsChanging}
+        />
+      ) : (
+        <UserNickChange
+          before={userInfo.nickname}
+          changeSetter={setIsChanging}
+          userInfo={userInfo}
+          setUserInfo={setUserInfo}
+        />
+      )}
+      {/* 사용자 정보 - 전화번호 */}
+      <UserContent title="전화번호" content={userInfo.phoneNumber} />
+      {/* 사용자 정보 - 이메일 */}
+      <UserContent title="이메일" content={userInfo.username} />
+    </div>
   );
 };
 
