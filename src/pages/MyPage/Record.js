3451c9e7353b4f062c69619f5805dff30cbe53b2
yh-project <yoha6865@gmail.com>
2023-11-02 11:24:34 +0900

    Fix/#137: 마이페이지 내역 조회 Fix
    UI 크기 조정
     - 크기가 작아짐에 따라 최근 본 차량 조회 배열 변경
     - 최근 본 차량이 없는 경우 멘트로 알려줌
    
    SideMenu 를 위한 id 추가

--- src/pages/MyPage/Record.js
+++ src/pages/MyPage/Record.js
@@ -37,46 +37,47 @@ const Record = () => {
     }
   }, [numberList]);
   return (
-    <>
-      <div className="flex flex-col items-center w-full py-8 mt-12 bg-sky-50 rounded-2xl shadow-figma">
-        {/* 타이틀 */}
-        <div className="w-[1010px] h-[70px] flex justify-between items-center">
-          <span className="text-blue-800 text-[45px] font-extrabold">
-            내역 조회
-          </span>
-        </div>
-        {/* 포인트 사용 내역 */}
-        <div className="w-[1010px] h-40 bg-white rounded-2xl flex items-center justify-between px-8 mt-7">
-          <div className="flex flex-col justify-between h-24 text-2xl font-bold w-fit">
-            <div className="text-slate-400">포인트 사용 내역</div>
-            <div className="flex items-center justify-between w-full text-2xl font-bold">
-              현재 포인트 : {userPoint}
-            </div>
-          </div>
-          <button
-            className="w-56 h-16 text-2xl font-bold bg-sky-200 rounded-2xl"
-            onClick={() => popUpPoint.toggle()}
-          >
-            사용 내역 확인
-          </button>
-        </div>
-        {/* 예약 내역 조회 */}
-        <div className="w-[1010px] h-40 bg-white rounded-2xl flex items-center px-8 mt-7">
-          <div className="flex items-center justify-between w-full h-24 text-2xl font-bold">
-            <div>예약 내역 조회</div>
-            <button
-              className="w-56 h-16 text-2xl font-bold bg-sky-200 rounded-2xl"
-              onClick={() => popUpResv.toggle()}
-            >
-              예약 내역 확인
-            </button>
+    <div
+      className="flex flex-col items-center w-full py-8 mt-12 bg-sky-50 rounded-2xl shadow-figma"
+      id="MyPage/Record"
+    >
+      {/* 타이틀 */}
+      <div className="w-[700px] h-[35px] flex justify-between items-center">
+        <span className="text-blue-800 text-[30px] font-extrabold">
+          내역 조회
+        </span>
+      </div>
+      {/* 포인트 사용 내역 */}
+      <div className="w-[700px] h-24 bg-white rounded-2xl flex items-center justify-between px-8 py-[15px] mt-4">
+        <div className="flex flex-col justify-between h-full font-bold">
+          <div className="text-slate-400">포인트 사용 내역</div>
+          <div className="flex items-center justify-between w-full text-xl">
+            현재 포인트 : {userPoint}
           </div>
         </div>
-        {/* 최근 본 차량 조회 */}
-        <div className="w-[1010px] bg-white rounded-2xl flex items-center px-8 py-8 mt-7">
-          <div className="flex flex-col justify-between w-full text-2xl font-bold">
-            최근 본 차량 조회
-            <div className="grid w-full grid-cols-3 px-8 pb-8 mt-4 bg-blue-100 rounded-2xl">
+        <button
+          className="text-lg font-bold w-36 h-11 bg-sky-200 rounded-xl hover:shadow-figma"
+          onClick={() => popUpPoint.toggle()}
+        >
+          사용 내역 확인
+        </button>
+      </div>
+      {/* 예약 내역 조회 */}
+      <div className="w-[700px] h-24 bg-white rounded-2xl flex items-center justify-between px-8 py-[15px] mt-4">
+        <div className="text-xl font-bold">예약 내역 조회</div>
+        <button
+          className="text-lg font-bold w-36 h-11 bg-sky-200 rounded-xl hover:shadow-figma"
+          onClick={() => popUpResv.toggle()}
+        >
+          예약 내역 확인
+        </button>
+      </div>
+      {/* 최근 본 차량 조회 */}
+      <div className="w-[700px] bg-white rounded-2xl flex items-center px-8 py-[15px] mt-4">
+        <div className="flex flex-col justify-between w-full text-xl font-bold">
+          최근 본 차량 조회
+          {recent.length !== 0 ? (
+            <div className="grid w-full grid-cols-2 px-8 mt-4 bg-blue-100 gap-y-4 rounded-2xl">
               {recent.map((v, i) => {
                 return (
                   <CarCard
@@ -91,10 +92,14 @@ const Record = () => {
                 );
               })}
             </div>
-          </div>
+          ) : (
+            <div className="flex items-center justify-center w-full h-24 mt-4 text-xl font-bold bg-blue-100 rounded-2xl">
+              최근 본 차량이 없습니다
+            </div>
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
