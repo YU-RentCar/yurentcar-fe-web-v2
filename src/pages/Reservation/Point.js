f03059c79c769d719b2653c76f478b6295910bd0
yh-project <yoha6865@gmail.com>
2023-11-02 18:21:26 +0900

    Fix/#139: 포인트 사용 UI 수정
    컨텐츠 박스 너비 수정
    
    텍스트 글자 크기 수정

--- src/pages/Reservation/Point.js
+++ src/pages/Reservation/Point.js
@@ -29,21 +29,21 @@ const Point = React.memo(() => {
       </div>
       <div className="w-[750px] h-28 flex justify-between items-center mt-4">
         {/* 보유 포인트 */}
-        <div className="w-[300px] h-full rounded-2xl bg-white flex flex-col justify-around pl-4 py-2">
-          <span className="text-base font-semibold text-slate-600">
+        <div className="w-[350px] h-full rounded-2xl bg-white flex flex-col justify-around pl-4 py-2">
+          <span className="text-lg font-semibold text-slate-600">
             현재 포인트
           </span>
-          <span className="text-xl font-bold">{userPoint}P</span>
+          <span className="text-2xl font-bold">{userPoint}P</span>
         </div>
         {/* 포인트 사용 */}
-        <div className="w-[300px] h-full rounded-2xl bg-white flex flex-col justify-around items-center py-2">
-          <div className="flex w-[280px] items-center justify-between text-base font-semibold">
+        <div className="w-[350px] h-full rounded-2xl bg-white flex flex-col justify-around items-center py-2">
+          <div className="flex w-[330px] items-center justify-between text-lg font-semibold">
             <span className="text-slate-600">사용 포인트</span>
-            <span id="ment" className="text-lg text-red-500"></span>
+            <span id="ment" className="text-red-500"></span>
           </div>
           <input
             type="number"
-            className="w-[280px] h-10 text-xl text-black font-bold border-2 border-black rounded-2xl px-2 placeholder:text-lg"
+            className="w-[280px] h-10 text-xl text-black font-bold border-2 border-black rounded-lg px-2 placeholder:text-lg"
             placeholder="사용할 포인트를 입력해주세요."
             onChange={(e) => {
               // 입력된 값에 따른 멘트 변화
