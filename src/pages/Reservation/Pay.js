488543b8f0f37a60752a09f490dbf2cbd4c0ff6d
yh-project <yoha6865@gmail.com>
2023-11-15 12:44:20 +0900

    Feat/#169: 카카오페이 결제 테스트
    카카오페이 결제 테스트

--- src/pages/Reservation/Pay.js
+++ src/pages/Reservation/Pay.js
@@ -1,4 +1,5 @@
 import { resvRent } from "api/reservationAxios";
+import dayjs from "dayjs";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { rentAtom } from "recoil/rentAtom";
@@ -60,6 +61,7 @@ const Pay = () => {
             else if (rentInfo.drivers.length === 0)
               alert.onAndOff("1명 이상의 운전자를 등록해주세요");
             else {
+              const IMP = window.IMP;
               const data = {
                 carNumber: rentInfo.carNumber,
                 startDate: rentInfo.startDate,
@@ -70,13 +72,40 @@ const Pay = () => {
                 reason: "차량 예약 사용",
                 drivers: rentInfo.drivers,
               };
-              resvRent(data)
+
+              // 구매 클릭시 한번 더 확인하기
+              IMP.init("imp71037182"); // 가맹점 식별코드
+              IMP.request_pay(
+                {
+                  pg: "kakaopay.TC0ONETIME", // PG사 코드표에서 선택
+                  pay_method: "card", // 결제 방식
+                  merchant_uid:
+                    "IMP" + dayjs(new Date()).format("YYYYMMDDHHmmss"), // 결제 고유 번호
+                  name: data.carNumber, // 제품명
+                  amount: 100, // 가격
+                  //구매자 정보 ↓
+                  buyer_email: `yoha6865@naver.com`,
+                  buyer_name: `최요하`,
+                },
+                async function (rsp) {
+                  // callback
+                  if (rsp.success) {
+                    //결제 성공시
+                    console.log(rsp);
+                    // axios
+                  } else if (!rsp.success) {
+                    // 결제 실패시
+                    alert(rsp.error_msg);
+                  }
+                }
+              );
+              /*resvRent(data)
                 .then((response) => {
                   nav("/mypage");
                 })
                 .catch((error) =>
                   console.log("예약 / 예약에러 : ", error.response)
-                );
+                );*/
             }
           }}
         >
